beec5754c545c6cbb5aecbdf2c890849ecf17644
Miguel Castro Franco <miguelangelcf52@gmail.com>
2025-01-28 14:20:07 -0500

    first load
    first load with architecture to start the project

---
+++ src/main/java/com/medicalsoftcontable/medicalsoftcontable/MedicalsoftcontableApplication.java
@@ -0,0 +1,13 @@
+package com.medicalsoftcontable.medicalsoftcontable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalsoftcontableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalsoftcontableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicalsoftcontable/medicalsoftcontable/MedicalsoftcontableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalsoftcontable.medicalsoftcontable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalsoftcontableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
